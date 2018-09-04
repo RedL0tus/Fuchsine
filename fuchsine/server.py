0de091dc049680475d1a4454fc445ea4cc0df573
Kay <i@v2bv.net>
2018-09-03 17:46:46 -0700

    template: Use MDUI

--- fuchsine/server.py
+++ fuchsine/server.py
@@ -14,17 +14,12 @@ from enum import Enum
 from . import __version__
 from .template import *
 
-# Type of a path
-class PathType(Enum):
-    Directory = 0
-    File = 1
-
 def get_path_type(path):
     """Determine what type of path it is"""
     if os.path.isfile(path):
-        return PathType.File
+        return "File"
     else:
-        return PathType.Directory
+        return "Directory"
 
 def generate_file_dict(path):
     """Generate a dictionary of files and directories in the given path"""
@@ -33,6 +28,7 @@ def generate_file_dict(path):
     for item in orig_list:
         file_dict[item] = dict()
         file_dict[item]['type'] = get_path_type(str(path) + str(item))
+        file_dict[item]['mtime'] = os.path.getmtime(str(path) + str(item))
     return file_dict
 
 def generate_http_response(body=None, status=None, headers=None):
@@ -52,11 +48,14 @@ class Server(bottle.Bottle):
         self.route('/files', callback=self.redirect_to_files)
         self.route('/files/', callback=self.route_files)
         self.route('/files/<file_path:path>', callback=self.route_files)
+        self.route('/assets/<file_path:path>', callback=self.serve_assets)
     
-    def render_index(self, path_type, real_path):
+    def render_index(self, file_path, path_type, real_path):
         """Render indexes"""
         # Initialize response
         source = dict()
+        source['path'] = file_path
+        source['base_url'] = self.config['DEFAULT']['base_url'] or str(self.config['DEFAULT']['host'] + self.config['DEFAULT']['port'])
         source['type'] = path_type
         source['files'] = generate_file_dict(real_path)
         # Render and return response
@@ -69,16 +68,19 @@ class Server(bottle.Bottle):
         real_path = self.config['DEFAULT']['root'] + "/" + str(file_path)
         # Get that kind of path it is
         path_type = get_path_type(real_path)
-        if path_type == PathType.File:
+        if path_type == "File":
             return bottle.static_file(file_path, root=self.config['DEFAULT']['root'], download=str(file_path))
         else:
             if os.path.isdir(real_path):
                 if (not file_path.endswith("/")) and (file_path != ""):
-                    bottle.redirect(file_path + "/")
-                return self.render_index(path_type, real_path)
+                    bottle.redirect("/files/" + file_path + "/")
+                return self.render_index(file_path, path_type, real_path)
             else:
                 return bottle.abort(404, "File not found")
         return bottle.abort(500, "Unknown server error")
+
+    def serve_assets(self, file_path):
+        return bottle.static_file(file_path, root=self.config['DEFAULT']['template'] + "/assets")
 
     def redirect_to_files(self):
         """Redirect to subdirectory 'files/'"""
