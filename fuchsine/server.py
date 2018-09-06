c66a605b6262670f17f9da4029318b374c6e5184
Kay <i@v2bv.net>
2018-09-05 21:35:44 -0700

    server: Add comments, clean up.

--- fuchsine/server.py
+++ fuchsine/server.py
@@ -31,45 +31,63 @@ def generate_http_response(body=None, status=None, headers=None):
     return bottle.HTTPResponse(body, status, **headers)
 
 class Server(bottle.Bottle):
+    """Fuchsine server main object (Bottle)"""
+
     def __init__(self, config):
-        """Setup bottle object"""
+        """Setup server"""
         super(Server, self).__init__()
+        # Save config
         self.config = config
         # Initialize index
         self.index = dict()
         self.build_index()
         # Routes
-        self.route('/', callback=self.route_files)
-        self.route('/<file_path:path>', callback=self.route_files)
+        self.route('/', callback=self.main_router)
+        self.route('/<file_path:path>', callback=self.main_router)
 
     class FileChangeHandler(FileSystemEventHandler):
+        """Just like its name"""
         def __init__(self, outer):
+            # Pass outer object inside
             self.upper = outer
 
         def on_modified(self, event):
+            """
+            Regenerate index when there are any modification in the watching directory
+            """
             self.upper.build_index()
 
     class FileChangeListener(threading.Thread):
+        """Second thread that listen to file system change"""
         def __init__(self, outer, handler):
             threading.Thread.__init__(self)
+            # Pass outer object and handler inside
             self.outer = outer
             self.handler = handler
 
         def run(self):
+            # Start watchdog
             observer = Observer()
             observer.schedule(self.handler, path=self.outer.config['DEFAULT']['root'], recursive=True)
             observer.start()
 
     def build_index(self):
+        """Build file index"""
         print("(Re)building index...")
+        # Create new dict, avoid overwriting existing index
         new_index = dict()
+        # Iterate through all files and directories in the annotated directory
         for path, subdirs, files in os.walk(self.config['DEFAULT']['root'], followlinks=True):
             for name in (files + subdirs):
+                # Get relative path
                 file_path = "/" + str(pathlib.PurePath(path, name) \
                     .relative_to(pathlib.PurePath( \
                         self.config["DEFAULT"]['root'])))
+                # Get real path
                 real_path = self.config['DEFAULT']['root'] + file_path
+                # Catch frequent errors
                 try:
+                    # Gather information needed
                     new_index[file_path] = dict()
                     new_index[file_path]['type'] = get_path_type(real_path)
                     new_index[file_path]['mtime'] = os.path.getmtime(real_path)
@@ -79,6 +97,7 @@ class Server(bottle.Bottle):
                     print("Found one missing file: " + real_path)
                 except OSError:
                     print("Error while creating index for: " + real_path)
+        # Replace index
         self.index = new_index
         print("Done.")
     
@@ -90,15 +109,27 @@ class Server(bottle.Bottle):
         source['path'] = file_path
         source['config'] = self.config['DEFAULT']
         source['files'] = dict()
+        path_found = False
         for path in self.index.keys():
-            if (path.startswith(file_path)) and ("/" not in path[len(file_path):]):
-                source['files'][path[len(file_path):]] = self.index[path]
-        # Render and return response
-        return generate_http_response(body=render_page(self.config['DEFAULT']['template'] + "/index.html", source))
+            if (path + "/").startswith(file_path):
+                path_found = True
+                if path.startswith(file_path) and ("/" not in path[len(file_path):]):
+                    source['files'][path[len(file_path):]] = self.index[path]
+        if path_found:
+            # Render and return response
+            return generate_http_response(body=render_page(self.config['DEFAULT']['template'] + "/index.html", source))
+        else:
+            return bottle.abort(404, "Path not found")
 
-    def route_files(self, file_path=None):
-        """Serve the files"""
+    def serve_assets(self, file_path):
+        """Serve template files"""
+        return bottle.static_file(file_path, root=self.config['DEFAULT']['template'])
+
+    def main_router(self, file_path=None):
+        """Main router"""
         file_path = file_path or ""
+        # Resource file routing
+        # TODO: Make this route customizable
         if file_path.startswith("fuchsine/"):
             return self.serve_assets(file_path[9:])
         # Get real path
@@ -106,18 +137,13 @@ class Server(bottle.Bottle):
         # Get that kind of path it is
         path_type = get_path_type(real_path)
         if path_type == "File":
+            # Direct serve the files
             return bottle.static_file(file_path, root=self.config['DEFAULT']['root'], download=str(file_path))
         else:
-            if os.path.isdir(real_path):
-                if (not file_path.endswith("/")) and (file_path != ""):
-                    bottle.redirect(self.config['DEFAULT']['base_url'] + '/' + file_path + '/')
-                return self.render_index(file_path)
-            else:
-                return bottle.abort(404, "File not found")
+            if (not file_path.endswith("/")) and (file_path != ""):
+                bottle.redirect(self.config['DEFAULT']['base_url'] + '/' + file_path + '/')
+            return self.render_index(file_path)
         return bottle.abort(500, "Unknown server error")
-
-    def serve_assets(self, file_path):
-        return bottle.static_file(file_path, root=self.config['DEFAULT']['template'])
 
     def rocknroll(self):
         handler = self.FileChangeHandler(self)
